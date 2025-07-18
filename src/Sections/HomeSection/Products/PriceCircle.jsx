e22e66bdd8e811623aa2d1299637f26a45db6c26
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-07-18 22:01:23 +0500

    new

--- src/Sections/HomeSection/Products/PriceCircle.jsx
+++ src/Sections/HomeSection/Products/PriceCircle.jsx
@@ -4,7 +4,7 @@ const PriceCircle=()=> {
   return (
     <>
     <div className="circle absolute -translate-y-24 -translate-x-3 w-12 h-12 rounded-[100%] flex justify-center items-center bg-secondary mx-auto">
-        <div className="price text-center text-xs text-white">$999</div>
+        <div className="price text-center text-xs text-white">$9999</div>
     </div>
     </>
   )
