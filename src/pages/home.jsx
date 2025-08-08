b1c21006b994f250da718c6c3477837f0ebde216
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-08-08 08:20:34 +0500

    product grids

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,12 +4,14 @@ import Category2 from "../Sections/HomeSection/CategorySection/Category.jsx";
 import FeatureHighlight from "../Sections/HomeSection/FetaureHighlight/FeatureHighlight.jsx";
 import ScrollingBanner from "../Sections/HomeSection/ScrollingItems/BannnerScrolling.jsx";
 import CustomerLove from "../Sections/HomeSection/Customerloveus/custloveus.jsx";
+import ProductGrid from "../Sections/HomeSection/Toptrending/index.jsx";
 const Home = () => {
   return (
     <>
     <Herosection />
     <Category2 />
     <Products />
+    <ProductGrid />
     <CustomerLove />
     <FeatureHighlight />
     {/* <ScrollingBanner /> */}
