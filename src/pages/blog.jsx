13215ce1f8cf4275716583d3308f9773ee6a69dc
subhanrizwan <subhanahmed444sherazi@gmail.com>
2025-11-20 05:59:10 +0500

    new update

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,6 +1,6 @@
 const blog=()=> {
   return (
-    <div><h1 className='text-4xl text-center fw-bold'>Blog</h1></div>
+    <div><h1 className='text-4xl text-center fw-bold'>Blogs</h1></div>
   )
 }
 export default blog
