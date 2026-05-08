0de2211aef180a9d26ee48c5bb57c75420f709cd
subhanrizwan <subhanahmed444sherazi@gmail.com>
2026-05-08 05:27:24 +0500

    update

--- src/components/Header/topHeader.jsx
+++ src/components/Header/topHeader.jsx
@@ -10,7 +10,7 @@ function TopHeader() {
 
   return (
     <>
-      <section className='top-header w-full bg-gradient-to-r from-[#03ADA4] to-white text-gray-800 py-3 text-center text-xs md:text-sm'>
+      <section className='top-header bg-gradient-to-r from-[#03ADA4] to-white text-gray-800 py-3 text-center text-xs md:text-sm'>
         <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <b className='cursor-pointer' onClick={ShopNow}> Shop Now</b>  </p>
       </section>
     </>
